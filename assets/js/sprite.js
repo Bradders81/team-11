71cb86de6189b7f36e21adcb6d3d0e8ba573febc
steveforrest <steveforrest_1@hotmail.com>
2021-08-07 21:37:01 +0000

    finished for the night sprite moves but gets stuck out when of bound

--- assets/js/sprite.js
+++ assets/js/sprite.js
@@ -1,41 +1,43 @@
 let sprite = document.getElementById('sprite');
-let spritePosition = sprite.innerHTML.offsetTop;
+let move = 3;
 
-console.log(spritePosition)
+window.addEventListener('keydown', (e) =>{
+    if(parseInt(sprite.style.left) >= 0 && parseInt(sprite.style.left) <= 855 && parseInt(sprite.style.top) >= 0 && parseInt(sprite.style.top) <= 650){
+        switch (e.key){
+            case 'ArrowLeft':
+            sprite.style.left = parseInt(sprite.style.left) - move + 'px';
+            console.log('left')
+            break;
+        case 'ArrowRight':
+            sprite.style.left = parseInt(sprite.style.left) + move + 'px';
+            console.log('right')
+            break;
+        case 'ArrowUp':
+            sprite.style.top = parseInt(sprite.style.top) - move + 'px';
+            console.log('top')
+            break;    
+        case 'ArrowDown':
+            sprite.style.top = parseInt(sprite.style.top) + move + 'px';
+            console.log('down')
+            break;
+            }
+    }else if(parseInt(sprite.style.left) < 0){
+        sprite.style.left = 0;
+        console.log('Hit left');
+    }else if(parseInt(sprite.style.top) < 1){
+        parseInt(sprite.style.top) = 5;
+        console.log(sprite.style.top)
+        console.log('Hit top');
+    }else if(parseInt(sprite.style.left) > 854){
+        sprite.style.left = 854;
+        console.log('Hit right');
+    }else if(parseInt(sprite.style.top) > 649){
+        sprite.style.top = 649;
+        }
+})
 
-/* load images
-const images = [];
-images.length = 6;
-
-for(var i = 1; i < images.length ; i++){
-    images[i] = new Image();
-    images[i].src = 'sprite'+'i.toString()'+'png';
-}
-
-function moveRight(){
-
-}
-*/
-sprite.addEventListener("keydown", function(event) {
-    event.preventDefault();
-    const key = event.key; // "ArrowRight", "ArrowLeft", "ArrowUp", or "ArrowDown"
-    switch (key) { // change to event.key to key to use the above variable
-      case "ArrowLeft":
-        // Left pressed
-        <do something>
-        break;
-      case "ArrowRight":
-        // Right pressed
-        <do something>
-        break;
-      case "ArrowUp":
-        // Up pressed
-        sprite.top = +50px;
-
-        break;
-      case "ArrowDown":
-        // Down pressed
-        <do something>
-        break;
-    }
-  });
+window.addEventListener('load', ()=>{
+    sprite.style.position = 'relative';
+    sprite.style.left = '10px'
+    sprite.style.top = '150px'
+   })
