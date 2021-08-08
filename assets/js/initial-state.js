e108b5da9f7ba0e469015fbe6a8ae3abddc0dcde
AwsSG <aws.sabah.gheni@gmail.com>
2021-08-08 23:21:20 +0000

    added game over with reset function

--- assets/js/initial-state.js
+++ assets/js/initial-state.js
@@ -1,7 +1,7 @@
 let sprite = document.getElementById('sprite');
 let resetPoints = document.getElementById('timer')
 
-function initial-state(){
+function initialState(){
     Sprite.style.display.innerHTML = none;
     resetPoints.innerHTML = 0;
     gameBox.removeChild(newWall);
