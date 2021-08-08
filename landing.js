14b10ee4078eeb19dc78cc8463cb61c9bb5fe9dd
AthAjuong <78806711+AthAjuong@users.noreply.github.com>
2021-08-08 12:32:36 +0100

    Landing (#15)
    * added game title, menu and modals
    
    * created js file for landing page
    
    * renamed files to landing, will be index in main branch
    
    * import font, add standard rules to landing page
    
    * added rules and basic animation for the game title. will convert to spans in js
    
    * create size rules for main menu section
    
    * create rules for menu items, buttons and play button on landing menu
    
    * added styling rules for the modals after each button is pressed
    
    * created simple animations for game title and menu items
    
    * target buttons, modals, and create strings for each modal's info
    
    * created handlebuttonevents() will change modal content according to button pressed
    
    * add event listeners to make modals disappear
    
    * created a function to split game title into individual spans

---
+++ landing.js
@@ -0,0 +1,71 @@
+// modal string content
+var about_info = "Hunger dash is a lovely game created by Team 11 of Code Institute's August 2021 Hackathon.";
+var rules_info = "The Rules of Hunger Dash are very simple.";
+var leaderboard_info = "Here is the current Hunger Dash leaderboard!";
+
+//game name for H1
+const my_title = "Hunger Dash";
+
+// modal variables
+const the_modal = document.getElementById('modal');
+const modal_header = document.querySelector('.modal-header');
+const modal_body = document.querySelector('.modal-body');
+
+//open modal btns
+const open_modal = document.getElementsByClassName('open-modal');
+
+//close modal btns
+const close_btn = document.getElementById('close-btn');
+const x_out_btn = document.getElementById('x-out');
+
+//function to handle button event listeners
+function handleButtonEvents() {
+
+    //event listeners change info for about, rules and leader btns.
+    open_modal[0].addEventListener('click', function() {
+        the_modal.style.display = "block";
+        modal_header.firstElementChild.innerHTML = "About";
+        modal_body.innerHTML = `<p>${about_info}</p>`;
+    });
+
+    open_modal[1].addEventListener('click', function() {
+        the_modal.style.display = "block";
+        modal_header.firstElementChild.innerHTML = "Rules";
+        modal_body.innerHTML = `<p>${rules_info}</p>`;
+    });
+
+    open_modal[2].addEventListener('click', function() {
+        the_modal.style.display = "block";
+        modal_header.firstElementChild.innerHTML = "Leaderboard";
+        modal_body.innerHTML = `<p>${leaderboard_info}</p>`;
+    });
+
+    //event listeners to close the modals
+    close_btn.addEventListener('click', function() {
+        the_modal.style.display = "none";
+    });
+
+    x_out_btn.addEventListener('click', function() {
+        the_modal.style.display = "none";
+    });
+
+    //if user clicks outside the modal box on the background
+    window.onclick = function(event) {
+        if(event.target == the_modal) {
+            the_modal.style.display = "none";
+        }
+    }
+}
+
+//create a function to separate title letters into individual spans
+function splitGameTitle() {
+    const separated_title = my_title.split("");
+    const heading = document.querySelector('h1');
+
+    for(let i=0; i < separated_title.length; i++) {
+        heading.innerHTML += "<span>"+separated_title[i]+"</span>"
+    }
+}
+
+handleButtonEvents();
+splitGameTitle();
