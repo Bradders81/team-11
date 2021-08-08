29f9c09dd681600239e5738e653fdc1fdc8a7946
AthAjuong <a.ajuong@gmail.com>
2021-08-08 10:32:49 +0000

    target buttons, modals, and create strings for each modal's info

---
+++ landing.js
@@ -0,0 +1,16 @@
+// modal string content
+var about_info = "Hunger dash is a lovely game created by Team 11 of Code Institute's August 2021 Hackathon.";
+var rules_info = "The Rules of Hunger Dash are very simple.";
+var leaderboard_info = "Here is the current Hunger Dash leaderboard!";
+
+// modal variables
+const the_modal = document.getElementById('modal');
+const modal_header = document.querySelector('.modal-header');
+const modal_body = document.querySelector('.modal-body');
+
+//open modal btns
+const open_modal = document.getElementsByClassName('open-modal');
+
+//close modal btns
+const close_btn = document.getElementById('close-btn');
+const x_out_btn = document.getElementById('x-out');
