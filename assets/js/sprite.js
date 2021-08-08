90b30e015cf7fe0cc908f3fd9377ca6b7d48b6ae
Bradders81 <p-bradley@live.co.uk>
2021-08-08 09:12:45 +0000

    Fixed bug wtih sprite

--- assets/js/sprite.js
+++ assets/js/sprite.js
@@ -1,43 +1,42 @@
 let sprite = document.getElementById('sprite');
 let move = 3;
 
-window.addEventListener('keydown', (e) =>{
-    if(parseInt(sprite.style.left) >= 0 && parseInt(sprite.style.left) <= 855 && parseInt(sprite.style.top) >= 0 && parseInt(sprite.style.top) <= 650){
-        switch (e.key){
+window.addEventListener('keydown', (e) => {
+    if (parseInt(sprite.style.left) >= 0 && parseInt(sprite.style.left) <= 855 && parseInt(sprite.style.top) >= 0 && parseInt(sprite.style.top) < 651) {
+        switch (e.key) {
             case 'ArrowLeft':
-            sprite.style.left = parseInt(sprite.style.left) - move + 'px';
-            console.log('left')
-            break;
-        case 'ArrowRight':
-            sprite.style.left = parseInt(sprite.style.left) + move + 'px';
-            console.log('right')
-            break;
-        case 'ArrowUp':
-            sprite.style.top = parseInt(sprite.style.top) - move + 'px';
-            console.log('top')
-            break;    
-        case 'ArrowDown':
-            sprite.style.top = parseInt(sprite.style.top) + move + 'px';
-            console.log('down')
-            break;
-            }
-    }else if(parseInt(sprite.style.left) < 0){
-        sprite.style.left = 0;
-        console.log('Hit left');
-    }else if(parseInt(sprite.style.top) < 1){
-        parseInt(sprite.style.top) = 5;
-        console.log(sprite.style.top)
-        console.log('Hit top');
-    }else if(parseInt(sprite.style.left) > 854){
-        sprite.style.left = 854;
-        console.log('Hit right');
-    }else if(parseInt(sprite.style.top) > 649){
-        sprite.style.top = 649;
+                sprite.style.left = parseInt(sprite.style.left) - move + 'px';
+                console.log('left')
+                break;
+            case 'ArrowRight':
+                sprite.style.left = parseInt(sprite.style.left) + move + 'px';
+                console.log('right')
+                break;
+            case 'ArrowUp':
+                sprite.style.top = parseInt(sprite.style.top) - move + 'px';
+                console.log('top')
+                break;
+            case 'ArrowDown':
+                sprite.style.top = parseInt(sprite.style.top) + move + 'px';
+                console.log('down')
+                break;
         }
+    } else if (parseInt(sprite.style.left) < 0) {
+        sprite.style.left = 0 + 'px';
+
+    } else if (parseInt(sprite.style.top) < 0) {
+        sprite.style.top = 0 + 'px';
+
+    } else if (parseInt(sprite.style.left) > 0 && parseInt(sprite.style.left) >= 850) {
+        sprite.style.left = 850 + 'px';
+
+    } else if (parseInt(sprite.style.top) > 0 && parseInt(sprite.style.top) > 650) {
+        sprite.style.top = 650 + 'px';
+    }
 })
 
-window.addEventListener('load', ()=>{
+window.addEventListener('load', () => {
     sprite.style.position = 'relative';
-    sprite.style.left = '10px'
-    sprite.style.top = '150px'
-   })
+    sprite.style.left = '0px'
+    sprite.style.top = '300px'
+})
