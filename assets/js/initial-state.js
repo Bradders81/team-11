52094ba28066a01999f6e2cb0aa7dd869daeb8b8
steveforrest <steveforrest_1@hotmail.com>
2021-08-08 22:47:49 +0100

    Steves branch2 (#26)
    * changed sprite
    
    * crearted timer function
    
    * added function

---
+++ assets/js/initial-state.js
@@ -0,0 +1,8 @@
+let sprite = document.getElementById('sprite');
+let resetPoints = document.getElementById('timer')
+
+function initial-state(){
+    Sprite.style.display.innerHTML = none;
+    resetPoints.innerHTML = 0;
+    gameBox.removeChild(newWall);
+}
